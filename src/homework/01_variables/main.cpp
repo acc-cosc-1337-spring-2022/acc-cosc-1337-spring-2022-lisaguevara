bb2f26b2000d23ad642369701370c9f89279136b
Lisa Guevara <lisa.guevara@g.austincc.edu>
2022-02-03 14:08:48 -0600

    Configured file and added to main function block

--- src/homework/01_variables/main.cpp
+++ src/homework/01_variables/main.cpp
@@ -1,13 +1,22 @@
 ﻿//write include statements
-
+#include<iostream>
+#include "variables.h"
 
 //write namespace using statement for cout
-
-
+using std::cin, std::cout;
 
 int main()
 {
-	
+	int num;
+	cout<<"Enter a number: ";
+	cin>>num;
+
+	int result = multiply_numbers(num);
+	cout<<"Result with entered value: "<<result<<"\n";
+
+	int num1 = 4;
+	result = multiply_numbers(num1);
+	cout<<"Result with num1 variable: "<<result<<"\n";
 
 	return 0;
 }
