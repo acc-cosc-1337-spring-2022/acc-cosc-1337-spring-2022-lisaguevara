98b10c1c95ac709766b81a7c18dacbf25540c295
Lisa Guevara <lisa.guevara@g.austincc.edu>
2022-02-10 16:06:40 -0600

    Added 2 function blocks

--- src/homework/02_expressions/expressions.cpp
+++ src/homework/02_expressions/expressions.cpp
@@ -1,4 +1,5 @@
-#include "expressions.h"
+#include "../01_variables/variables.h"
+#include "../02_expressions/expressions.h"
 
 //example
 int add_numbers(int num1, int num2)
@@ -6,9 +7,12 @@ int add_numbers(int num1, int num2)
 	return num1 + num2;
 }
 
-//write code for multiply_numbers function here
-//function returns product of num1 and num2
+double get_sales_tax_amount(double meal_amount)
+{
+	return meal_amount * TAX_RATE;
+}
 
-
-
+double get_tip_amount(double meal_amount, double tip_rate) {
+	return meal_amount * tip_rate;
+}
 
