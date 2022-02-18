3721aa7f9fd9ab0059dcc7fed79d453d41599bc1
Lisa Guevara <lisa.guevara@g.austincc.edu>
2022-02-17 19:10:30 -0600

    Hw 3 Complete

--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,20 +1,13 @@
 #include<iostream>
-#include<string>
 using std::cout; using std::cin; using std::string;
 //write include statements
 #include "decisions.h"
 
 int main() 
 {
-	// Initialize constants for main menue
-	const int USE_IF = 1;
-	const int USE_SWITCH = 2;
-	const int EXIT = 3;
-
 	// Declare variables
 	int menu_choice;
 	int grade;
-	string letter_grade;
 
 	// Display main menu
 	cout << "\n\tMAIN MENU\t\n";
@@ -26,7 +19,7 @@ int main()
 	cin >> menu_choice;
 
 	// Gather grade input based on menu_choice
-	if (menu_choice == USE_IF || menu_choice == USE_SWITCH) {
+	if (menu_choice == 1 || menu_choice == 2) {
 		cout << "\nEnter a grade between 0-100: ";
 		cin >> grade;
 
@@ -37,21 +30,18 @@ int main()
 	}
 
 	switch (menu_choice) {
-		case USE_IF:
-			letter_grade = get_letter_grade_using_if(grade);
-			cout << "\n" << grade << " converts to letter grade: " << letter_grade;
+		case 1:
+			cout << "\n" << grade << " converts to letter grade: " << get_letter_grade_using_if(grade);
 			break;
-		case USE_SWITCH: 
-			letter_grade = get_letter_grade_using_switch(grade);
-			cout << "\n" << grade << " converts to letter grade: " << letter_grade;
+		case 2:
+			cout << "\n" << grade << " converts to letter grade: " << get_letter_grade_using_switch(grade);
 			break;
-		case EXIT: 
+		case 3: 
 		case -1:
 			cout << "Exiting program...";
 			break;
 		default: 
 			cout << "Menu choice must be between 1-3.\nExiting program...";
 	}
-
 	return 0;
 }
